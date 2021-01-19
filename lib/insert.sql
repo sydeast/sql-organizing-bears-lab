13d14eb85b322f81ebf10897c1d3063617c4bdce
Sydney Daniels <sydeast@gmail.com>
2021-01-19 11:38:29 -0600

    commit tree d8e8b1a5d04548650ba0ad8d870d1fa95c002c84

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears VALUES (1, 'Mr. Chocolate', 3, 'M', 'brown', 'friendly', 1);
+INSERT INTO bears VALUES (2, 'Rowdy', 7, 'F', 'grey', 'nice', 1);
+INSERT INTO bears VALUES (3, 'Tabitha', 2, 'F', 'reddish', 'melancholic', 1);
+INSERT INTO bears VALUES (4, 'Sergeant Brown', 17, 'M', 'blond', 'outgoing', 0);
+INSERT INTO bears VALUES (5, 'Melissa', 9, 'F', 'white', 'gentle', 1);
+INSERT INTO bears VALUES (6, 'Grinch', 5, 'M', 'cinnamon', 'mean' 1);
+INSERT INTO bears VALUES (7, 'Wendy', 9, 'M', 'black', 'sweet', 1);
+INSERT INTO bears VALUES (8, null, 6, 'F', 'blue-grey', 'territorial' 0);
